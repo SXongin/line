cefd603080c18fdcc295bafba5d76710d2610021
SXongin <e2581108847@outlook.com>
2018-03-25 17:27:10 +0800

    interger bresenham algorithm.

--- line_bresenham.c
+++ line_bresenham.c
@@ -16,12 +16,12 @@ void setpixel(int x, int y) {
 void bresenham(int x0, int y0, int x1, int y1) {
     int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
     int dy = abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
-    int err = (dx > dy ? dx : -dy) / 2;
+    int err = (dx > dy ? -dx : dy);
 
     while (setpixel(x0, y0), x0 != x1 || y0 != y1) {
         int e2 = err;
-        if (e2 > -dx) { err -= dy; x0 += sx; }
-        if (e2 <  dy) { err += dx; y0 += sy; }
+        if (e2 < 2*dx) { err += 2*dy; x0 += sx; }
+        if (e2 > -2*dy) { err -= 2*dx; y0 += sy; }
     }
 }
 
